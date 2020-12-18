334180840726d7ef3183c57feb2fa254a432a4e7
Subhalingam D <subhalingam.d@gmail.com>
2020-12-18 12:35:28 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+# from scipy import stats
+
+
+def rank(data,filters={},percentile=0.95):
+	"""
+	IMDb Ranking function as stated in https://en.wikipedia.org/wiki/IMDb#Rankings
+	"""
+	for cat_class,cat_type in filters.items():
+		if cat_class == 'genres':
+			s = data.apply(lambda x: pd.Series(x[cat_class]),axis=1).stack().reset_index(level=1, drop=True)
+			s.name = cat_class
+			data = data.drop(cat_class, axis=1).join(s)
+		data = data[data[cat_class] == cat_type]
+
+	m = data[data['vote_count'].notnull()]['vote_count'].astype('int').quantile(percentile)
+	C = data[data['vote_average'].notnull()]['vote_average'].astype('int').mean()
+
+	
+
+	qual = data[(data['vote_count'] >= m) & (data['vote_count'].notnull()) & (data['vote_average'].notnull())][['title', 'year', 'vote_count', 'vote_average', 'popularity']]
+	qual['vote_count'], qual['vote_average'] = qual['vote_count'].astype('int'), qual['vote_average'].astype('int')
+
+
+	qual['rating'] = qual.apply(lambda x: (x['vote_average']*x['vote_count'] + m*C)/(x['vote_count']+m), axis=1)
+	qual = qual.sort_values('rating', ascending=False)
+	
+	#return (R*v + C*m)/(v+m)
+	return qual
+
