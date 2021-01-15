abbbbf9e30ed5b08190910c7c410389950431977
Archit Bansal <45760916+architbansal28@users.noreply.github.com>
2021-01-15 12:55:54 +0530

    Update content_based_recommender.py

--- content_based_recommender.py
+++ content_based_recommender.py
@@ -66,7 +66,19 @@ def get_recommendations_movie_desc(title):
 	sim_scores = sim_scores[1:31]
 	movie_indices = [i[0] for i in sim_scores]
 	
-	return titles.iloc[movie_indices]
+	movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year']]
+	vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
+	vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('float')
+#	C = vote_averages.mean()
+#	m = vote_counts.quantile(0.60)
+	qual = movies[(movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
+	qual['vote_count'] = qual['vote_count'].astype('int')
+	qual['vote_average'] = qual['vote_average'].astype('float')
+#	qual['wr'] = qual.apply(lambda x: weighted_rating(x,m,C), axis=1)
+#	qual = qual.sort_values('wr', ascending=False).head(10)
+	return qual
+
+#	return titles.iloc[movie_indices]
 
 def get_director(x):
 	for i in x:
